1874c892638c4cf4854aa3c44c48ac29acfc68b0
facundo-12bit <81126072+facundo-12bit@users.noreply.github.com>
2022-03-12 16:32:03 -0300

    Add files via upload

---
+++ lightbox.js
@@ -0,0 +1,27 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imageneslight = document.querySelector('.agregar-imagen')
+const contenedorlight = document.querySelector('.imagen-ligth')
+const hamburger1 = document.querySelector('.hamburger');
+
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+contenedorlight.addEventListener('click', (e)=>{
+    if(e.target !== imageneslight){
+        contenedorlight.classList.toggle('show')
+        imageneslight.classList.toggle('showImage')
+        hamburger1.style.opacity = '1'
+    }
+})
+
+
+const aparecerImagen = (imagen)=> {
+    imageneslight.src = imagen;
+    contenedorlight.classList.toggle('show')
+    imageneslight.classList.toggle('showImage')
+    hamburger1.style.opacity = '0'
+}
